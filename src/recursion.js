a261d330351bc973bd47c44ef3be143af96ab350
Seung Don Choi <dchoifor2@gmail.com>
2021-04-20 18:06:34 -0700

    passed one more test case

--- src/recursion.js
+++ src/recursion.js
@@ -132,9 +132,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
   if (string.length < 2) {
     return true;
   }
+  if (string[0] !== string[string.length -1]) {
+    return false;
+  }
+  return palindrome(string.slice(1, string.length - 1))
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
